81caaf030ddbcb30ec281405c4b06d6775d26231
Andy <andrewballester@gmail.com>
2023-06-09 19:48:38 -0700

    list files

--- image.go
+++ image.go
@@ -54,7 +54,43 @@ func createImage(text string) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+func ListFiles(directory string) ([]string, error) {
+	fileInfo, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []string
+	for _, file := range fileInfo {
+		files = append(files, file.Name())
+	}
+
+	return files, nil
+}
+
 func main() {
+	// Create a file server which serves files out of the "images" directory.
+	// Note: The file server is wrapped in the http.StripPrefix function to
+	// remove the "/images" prefix when looking for files.
+	fs := http.StripPrefix("/images/", http.FileServer(http.Dir("/images")))
+	http.Handle("/images/", fs)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, server is running!"))
+	})
+
+	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		files, err := ListFiles("images")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			w.Write([]byte("<a href='" + file + "'>" + file + "</a><br>" + "\n"))
+		}
+
+		//w.Write([]byte("Hello, server is running!"))
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
